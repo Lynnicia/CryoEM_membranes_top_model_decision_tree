76920aa26c3cd6b2b0f6458e668cd46292491356
Lynnicia <Lynnicia@users.noreply.github.com>
2026-04-11 15:42:28 -0400

    Update ULD_models.py

--- seed/ULD_models.py
+++ seed/ULD_models.py
@@ -12,7 +12,7 @@ def model_ULD_640():
     MODEL_PATH_u = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-U-Net-ULD.pt"
     MODEL_PATH_yv11 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-YOLOv11-ULD.pt"
     MODEL_PATH_y26 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-YOLO26-ULD.pt"
-    MODEL_PATH_d2 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-Detectron2-ULD.pt"
+    MODEL_PATH_d2 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-Detectron2-ULD.pth"
     MODEL_PATH_s3 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/640-SAM3-ULD.pt"
 
     return MODEL_PATH_u, MODEL_PATH_yv11, MODEL_PATH_y26, MODEL_PATH_d2, MODEL_PATH_s3
@@ -22,7 +22,7 @@ def model_ULD_1024():
     MODEL_PATH_u = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-U-Net-ULD.pt"
     MODEL_PATH_yv11 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-YOLOv11-ULD.pt"
     MODEL_PATH_y26 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-YOLO26-ULD.pt"
-    MODEL_PATH_d2 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-Detectron2-ULD.pt"
+    MODEL_PATH_d2 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-Detectron2-ULD.pth"
     MODEL_PATH_s3 = "/content/CryoEM_membranes_top_model_decision_tree/seed/models/1024-SAM3-ULD.pt"
 
     return MODEL_PATH_u, MODEL_PATH_yv11, MODEL_PATH_y26, MODEL_PATH_d2, MODEL_PATH_s3
